49ae31f1cb77b394b395ccd874c99c7a278d682b
Saúl Calderón <saulcalderon1408@gmail.com>
2022-10-29 22:24:18 -0600

    añadir nuevos mensajes de sweetalert

--- js/init.js
+++ js/init.js
@@ -4,8 +4,28 @@
     $('.parallax').parallax();
   }); // end of document ready
 
-  $('#login-btn').click(function () {
+  $('#btn-login').click(function () {
     swal('Bienvenido', '', 'success').then((value) => {
+      window.location.href = 'menu.html';
+    });
+  });
+
+  $('#btn-deposit').click(function () {
+    swal(
+      'Depósito exitoso',
+      'El depósito por la cantidad de $100 fue exitoso.',
+      'success'
+    ).then((value) => {
+      window.location.href = 'menu.html';
+    });
+  });
+
+  $('#btn-withdraw').click(function () {
+    swal(
+      'Retiro exitoso',
+      'El retiro por la cantidad de $100 fue exitoso.',
+      'success'
+    ).then((value) => {
       window.location.href = 'menu.html';
     });
   });
