4ff6292f992a15ec7430e3ac48a0305d8ac505b8
Saul Calderon <saulcalderon1408@gmail.com>
2022-11-27 21:50:45 -0600

    Add info charts

--- js/init.js
+++ js/init.js
@@ -21,6 +21,113 @@
       $('.no-cuenta').text(`Nº cuenta: ${user.noAccount}`);
       $('.balance-general').text(`Balance general: $${user.balance}`);
     }
+
+    if (window.location.pathname === '/transactions.html') {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const transactions = user.transactions;
+
+      let sumaDineroServiciosDeAgua = 0;
+      let sumaDineroServiciosDeLuz = 0;
+      let sumaDineroServiciosDeInternet = 0;
+
+      transactions.forEach((transaction) => {
+        if (transaction.type === 'service') {
+          if (transaction.category === 'agua') {
+            sumaDineroServiciosDeAgua += transaction.amount;
+          }
+
+          if (transaction.category === 'luz') {
+            sumaDineroServiciosDeLuz += transaction.amount;
+          }
+
+          if (transaction.category === 'internet') {
+            sumaDineroServiciosDeInternet += transaction.amount;
+          }
+        }
+      });
+
+      const ctx = document.getElementById('myChart').getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: [
+            'Servicio de Luz',
+            'Servicio de Agua',
+            'Servicio de Internet',
+          ],
+          datasets: [
+            {
+              label: 'Total de gastos en servicios',
+              data: [
+                sumaDineroServiciosDeLuz,
+                sumaDineroServiciosDeAgua,
+                sumaDineroServiciosDeInternet,
+              ],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+
+      let sumaDineroDepositos = 0;
+      let sumaDineroRetiros = 0;
+      let sumaDineroServicios = 0;
+
+      transactions.forEach((transaction) => {
+        if (transaction.type === 'deposit') {
+          sumaDineroDepositos += transaction.amount;
+        }
+
+        if (transaction.type === 'withdraw') {
+          sumaDineroRetiros += transaction.amount;
+        }
+
+        if (transaction.type === 'service') {
+          sumaDineroServicios += transaction.amount;
+        }
+      });
+
+      const ctx2 = document.getElementById('myChart2').getContext('2d');
+      new Chart(ctx2, {
+        type: 'pie',
+        data: {
+          labels: ['Depósitos', 'Retiros', 'Servicios'],
+          datasets: [
+            {
+              label: 'Total de transacciones en USD',
+              data: [
+                sumaDineroDepositos,
+                sumaDineroRetiros,
+                sumaDineroServicios,
+              ],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)',
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        },
+      });
+    }
   }); // end of document ready
 
   // information about the user
@@ -272,7 +379,7 @@
         user.balance = newBalance;
         user.transactions.push({
           type: 'service',
-          service: event.target.id,
+          category: event.target.id,
           noBill,
           amount: parseFloat(value),
           date: new Date(),
@@ -291,61 +398,4 @@
   });
 
   //TODO: Add path validation to prevent null error in console
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [
-        {
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-
-  const ctx2 = document.getElementById('myChart2').getContext('2d');
-  const myChar2 = new Chart(ctx2, {
-    type: 'pie',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow'],
-      datasets: [
-        {
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)',
-          ],
-          hoverOffset: 4,
-        },
-      ],
-    },
-  });
 })(jQuery); // end of jQuery name space
